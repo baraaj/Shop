7cdc44900b6002827e43f120002f84d5ad181146
baraaj <jeridibaraa@gmail.com>
2022-04-28 17:01:26 +0200

    feature

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,4 +1,6 @@
 const Users=require('../models/userModel')
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
 const userCtrl={
          register: async(req,res)=>{
              try{
@@ -7,6 +9,15 @@ const userCtrl={
               if(user)return res.status(400).json({msg:"The email already exists"})
               if (password.length <6)
               return res.status(400).json({msg:"Password is at least 6 chars long"})
+              //Password encryption
+              const passwordHash=await bcrypt.hash(password,10)
+              const newUser=new Users({name,email,password:passwordHash})
+              //save mongodb
+              await newUser.save()
+              
+              //then create jsonwebtoken to authentication
+              const accesstoken = createAccessToken({id: user._id})
+            const refreshtoken = createRefreshToken({id: user._id})
             res.json({msg:"Register Success"})
             
             }catch(err){
@@ -14,5 +25,11 @@ const userCtrl={
              }
          }
 
+}
+const createAccessToken = (user) =>{
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '11m'})
+}
+const createRefreshToken = (user) =>{
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
 }
 module.exports=userCtrl
