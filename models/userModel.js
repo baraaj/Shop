85db15f77443f9dd19d23f350804f9a16d8a262d
baraaj <jeridibaraa@gmail.com>
2022-05-03 21:11:53 +0200

    user feature

--- models/userModel.js
+++ models/userModel.js
@@ -9,13 +9,13 @@ const userSchema=new mongoose.Schema({
       email:{
         type:String,
         required:true,
-        trim:true
+        unique:true
 
     },
      password:{
         type:String,
         required:true,
-        trim:true
+         
 
     },
      role:{
