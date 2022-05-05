5586174b6f2e46756ff187948b9a9ae251097285
baraaj <jeridibaraa@gmail.com>
2022-05-06 01:35:05 +0200

    Product feature

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(fileUpload(
 //Routes
 app.use('/user',require('./routes/userRouter'))
 app.use('/api',require('./routes/categoryRouter'))
+app.use('/api',require('./routes/upload'))
 //Connection to Mongo
 const URI=process.env.MONGODB_URL
 mongoose.connect(URI,{
