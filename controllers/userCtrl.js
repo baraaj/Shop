6b40309422f00eb2ea03576500244153534df9e2
baraaj <jeridibaraa@gmail.com>
2022-05-05 11:59:32 +0200

    second commit

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -34,39 +34,41 @@ const userCtrl = {
   },
   login: async (req, res) => {
     try {
-        const {email, password } = req.body;
-      const user = await Users.findOne({ email })
-      if(!user) return res.status(400).json({ msg:"User does not exist"})
-      const isMatch=await bcrypt.compare(password,user.password)
-      if(!isMatch) return res.status(400).json({ msg:"Incorrect password"})
+      const { email, password } = req.body;
+      const user = await Users.findOne({ email });
+      if (!user) return res.status(400).json({ msg: "User does not exist" });
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ msg: "Incorrect password" });
       // res.json({msg:"Login success"})
-       const accesstoken = createAccessToken({ id: user._id });
+      const accesstoken = createAccessToken({ id: user._id });
       const refreshtoken = createRefreshToken({ id: user._id });
       res.cookie("refreshtoken", refreshtoken, {
         httpOnly: true,
         path: "/user/refresh_token",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
       });
-      res.json({accesstoken})
+      res.json({ accesstoken });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
   },
-  logout:async(req,res)=>{
-      try {
-          res.clearCookie('refresh_token',{path: "/user/refresh_token"})
-          return res.json({msg:"Logged out"})
-      } catch (err) {
-        return res.status(500).json({ msg: err.message });
-      }
+  logout: async (req, res) => {
+    try {
+      res.clearCookie("refresh_token", { path: "/user/refresh_token" });
+      return res.json({ msg: "Logged out" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
   },
-  getUser:async(req,res)=>{
-     try {
-         
-     } catch (err) {
-        return res.status(500).json({ msg: err.msg }); 
-     }
+  getUser: async (req, res) => {
+    try {
+      const user = await Users.findById(req.user.id).select("-password");
+      if (!user) return res.status(400).json({ msg: "User does not exist." });
 
+      res.json(user);
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
   },
   refreshToken: (req, res) => {
     try {
